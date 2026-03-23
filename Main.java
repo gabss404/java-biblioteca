729deda076a63e008a965fb808521cd263c254e0
Gabriela Costa <gabrielarocha29.gr56@gmail.com>
2026-03-23 20:23:47 -0300

    Refactor main menu and extract methods for actions

--- Main.java
+++ Main.java
@@ -1,54 +1,32 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Biblioteca biblioteca = new Biblioteca();
         Scanner sc = new Scanner(System.in);
         int escolha;
 
         do {
-            System.out.println("\n--- Biblioteca ---");
-            System.out.println("1 - Cadastrar Livro");
-            System.out.println("2 - Cadastrar Usuário");
-            System.out.println("3 - Emprestar Livro");
-            System.out.println("4 - Devolver Livro");
-            System.out.println("5 - Listar Livros");
-            System.out.println("0 - Sair");
-            System.out.print("Escolha uma opção: ");
+            exibirMenu();
             escolha = sc.nextInt();
-            sc.nextLine(); // Limpar buffer
+            sc.nextLine();
 
             switch (escolha) {
                 case 1:
-                    System.out.print("ID do livro: ");
-                    int idLivro = sc.nextInt();
-                    sc.nextLine();
-                    System.out.print("Título do livro: ");
-                    String titulo = sc.nextLine();
-                    System.out.print("Autor: ");
-                    String autor = sc.nextLine();
-                    biblioteca.cadastrarLivro(new Livro(idLivro, titulo, autor));
+                    cadastrarLivro(sc, biblioteca);
                     break;
                 case 2:
-                    System.out.print("ID do usuário: ");
-                    int idUsuario = sc.nextInt();
-                    sc.nextLine();
-                    System.out.print("Nome do usuário: ");
-                    String nome = sc.nextLine();
-                    biblioteca.cadastrarUsuario(new Usuario(idUsuario, nome));
+                    cadastrarUsuario(sc, biblioteca);
                     break;
                 case 3:
-                    System.out.print("ID do livro a emprestar: ");
-                    int idEmprestar = sc.nextInt();
-                    biblioteca.emprestarLivro(idEmprestar);
+                    emprestarLivro(sc, biblioteca);
                     break;
                 case 4:
-                    System.out.print("ID do livro a devolver: ");
-                    int idDevolver = sc.nextInt();
-                    biblioteca.devolverLivro(idDevolver);
+                    devolverLivro(sc, biblioteca);
                     break;
                 case 5:
-                    biblioteca.listarLivros();
+                    listarLivros(biblioteca);
                     break;
                 case 0:
                     System.out.println("Saindo...");
@@ -59,5 +37,76 @@ public class Main {
         } while (escolha != 0);
 
         sc.close();
+    }
+
+    private static void exibirMenu() {
+        System.out.println("\n--- Biblioteca ---");
+        System.out.println("1 - Cadastrar Livro");
+        System.out.println("2 - Cadastrar Usuário");
+        System.out.println("3 - Emprestar Livro");
+        System.out.println("4 - Devolver Livro");
+        System.out.println("5 - Listar Livros");
+        System.out.println("0 - Sair");
+        System.out.print("Escolha uma opção: ");
+    }
+
+    private static void cadastrarLivro(Scanner sc, Biblioteca biblioteca) {
+        System.out.print("ID do livro: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Título: ");
+        String titulo = sc.nextLine();
+
+        System.out.print("Autor: ");
+        String autor = sc.nextLine();
+
+        biblioteca.cadastrarLivro(new Livro(id, titulo, autor));
+        System.out.println("Livro cadastrado com sucesso!");
+    }
+
+    private static void cadastrarUsuario(Scanner sc, Biblioteca biblioteca) {
+        System.out.print("ID do usuário: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Nome: ");
+        String nome = sc.nextLine();
+
+        biblioteca.cadastrarUsuario(new Usuario(id, nome));
+        System.out.println("Usuário cadastrado com sucesso!");
+    }
+
+    private static void emprestarLivro(Scanner sc, Biblioteca biblioteca) {
+        System.out.print("ID do livro: ");
+        int id = sc.nextInt();
+
+        boolean sucesso = biblioteca.emprestarLivro(id);
+
+        if (sucesso) {
+            System.out.println("Livro emprestado com sucesso!");
+        } else {
+            System.out.println("Não foi possível emprestar o livro.");
+        }
+    }
+
+    private static void devolverLivro(Scanner sc, Biblioteca biblioteca) {
+        System.out.print("ID do livro: ");
+        int id = sc.nextInt();
+
+        boolean sucesso = biblioteca.devolverLivro(id);
+
+        if (sucesso) {
+            System.out.println("Livro devolvido com sucesso!");
+        } else {
+            System.out.println("Não foi possível devolver o livro.");
+        }
+    }
+
+    private static void listarLivros(Biblioteca biblioteca) {
+        System.out.println("\nLivros da Biblioteca:");
+        for (Livro livro : biblioteca.listarLivros()) {
+            System.out.println(livro);
+        }
     }
 }
